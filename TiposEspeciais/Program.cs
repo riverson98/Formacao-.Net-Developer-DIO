5afd325f244657a178893219b7907f2f7838fbdd
riverson98 <riversonvicente@gmail.com>
2023-11-29 15:37:48 -0300

    Tipos especiais - Tipos anonimos

--- TiposEspeciais/Program.cs
+++ TiposEspeciais/Program.cs
@@ -1,7 +1,13 @@
-﻿bool? desejaReceberEmail  = true;
+﻿// tipos nullable
+bool? desejaReceberEmail  = true;
 
 var mensagem = desejaReceberEmail.HasValue && desejaReceberEmail.Value 
     ? "O usuario optou por receber e-mail" 
     : "O usuario não respondeu ou optou por não receber e-mail";
     
 Console.WriteLine(mensagem);
+
+//tipos anonimos
+
+var tipoAnonimo = new { Nome = "Wei", Sobrenome = "Cheng", Altura = 1.68 };
+Console .WriteLine($"Nome: {tipoAnonimo.Nome}, Sobrenome: {tipoAnonimo.Sobrenome}, Altura: {tipoAnonimo.Altura}");

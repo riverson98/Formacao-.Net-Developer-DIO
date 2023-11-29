fdb4d2e6e5ffc729d18dd7b1a04480fb051ca650
riverson98 <riversonvicente@gmail.com>
2023-11-29 15:13:55 -0300

    TiposEspecias: Tipos nulos na pratica

---
+++ TiposEspeciais/Program.cs
@@ -0,0 +1,7 @@
+﻿bool? desejaReceberEmail  = true;
+
+var mensagem = desejaReceberEmail.HasValue && desejaReceberEmail.Value 
+    ? "O usuario optou por receber e-mail" 
+    : "O usuario não respondeu ou optou por não receber e-mail";
+    
+Console.WriteLine(mensagem);
